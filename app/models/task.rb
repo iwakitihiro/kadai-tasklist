f71059c17356246e60168188676e989f5c3b9fae
hiro.iwakiti <iwakitihiro392@gmail.com>
2021-01-12 11:35:54 +0000

    kadai-tasklist submit a

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  validates :status, presence: true,lenght: { maximum: 10 }
-  validates :content, presence: true, lenght: { maximum: 255 }
+  validates :status, presence: true,length: { maximum: 10 }
+  validates :content, presence: true, length: { maximum: 255 }
 end
 
